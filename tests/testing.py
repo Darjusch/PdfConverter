f8bea981f40324b4d4b24f922d37d4fd4572bbe8
darjusch.schrand <Darjusch.schrand@bonial.com>
2019-11-06 11:21:09 +0100

    Refactored and ajdusted unittests for the PageObject

--- tests/testing.py
+++ tests/testing.py
@@ -11,44 +11,49 @@ class PageObjectTest(unittest.TestCase):
 
     def setUp(self):
         self.main = MainWindow()
-        self.page_objects = self.main.pdf_to_push_button("../tests/test2.pdf")
+        self.page_objects = self.main.convert_pdf_pages_to_push_button("../tests/test2.pdf")
         self.page_object = self.page_objects[0]
         self.img = self.page_object.img
         self.rotation = self.page_object.rotation
         self.page = self.page_object.pdf_page
 
-    def test_image_is_QImage(self):
-        self.assertIsInstance(self.img, PySide2.QtGui.QImage)
+    def test_if_page_converts_to_qimage(self):
+        self.assertIsInstance(self.page_object.page_to_qimage(self.page_object.pdf_page), PySide2.QtGui.QImage)
 
-    def test_page_converts_to_image(self):
-        self.assertIsInstance(self.page_object.pageToImage(self.page_object.pdf_page), PySide2.QtGui.QImage)
+    def test_if_len_pdf_pages_equals_created_push_button(self):
+        self.assertEqual(len(self.page_objects), 4)
 
-    def test_pdf_to_push_button_len(self):
-        self.assertEqual(len(self.page_objects), 12)
-
-    def test_pdf_roates_left(self):
+    def test_if_pdf_rotates_left(self):
         self.page_object.rotate(-90)
         self.assertEqual(self.page_object.rotation, -90)
 
-    def test_pdf_rotates_right(self):
+    def test_if_pdf_rotates_right(self):
         self.page_object.rotate(90)
         self.assertEqual(self.page_object.rotation, 90)
 
-    def test_split_pdf_left(self):
+    def test_split_left_side_of_pdf_page(self):
         original_page = self.page_object.pdf_page
-        self.page_object.splitLeft()
+        self.page_object.split_left()
         after_split_page = self.page_object.pdf_page
         self.assertEqual(after_split_page, original_page)
 
-    def test_split_pdf_right(self):
+    def test_split_right_side_of_pdf_page(self):
         original_page = self.page_object.pdf_page
-        self.page_object.splitRight()
+        self.page_object.split_right()
         after_split_page = self.page_object.pdf_page
         self.assertEqual(after_split_page, original_page)
 
-    def test_convert_coordinates(self):
-        self.page_object.convert_coordinates(50, 50, 100, 100)
-        self.assertAlmostEqual(self.page_object.x1, 0.25)
+    def test_convert_coordinates_into_percentage(self):
+        self.page_object.convert_coordinates_into_percentage(50, 50, 100, 100)
+        self.assertListEqual(
+            [
+                round(self.page_object.x1, 3),
+                round(self.page_object.y1, 3),
+                round(self.page_object.x2, 3),
+                round(self.page_object.y2, 3)
+            ],
+            [0.136, 0.161, 0.407, 0.484]
+        )
 
 
 if __name__ == '__main__':
