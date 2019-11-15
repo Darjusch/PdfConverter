a96d1d9cbdb59b85b310d84a278c2bac5beb30db
darjusch.schrand <Darjusch.schrand@bonial.com>
2019-11-15 17:08:38 +0100

    Refactored positioning and deleting push_button from grid

--- pdf_converter/main.py
+++ pdf_converter/main.py
@@ -24,6 +24,10 @@ class MainWindow(QMainWindow):
         self.checked_objects = []
         self.pdf_path = ''
         self.pdf_reader = None
+        self.page_window = None
+        self.button_to_remove = None
+        self.row = 0
+        self.column = 0
         self.ui.openFileButton.clicked.connect(self.dialog_to_select_pdfs)
         self.ui.openFolderButton.clicked.connect(self.dialog_to_select_folder_with_pdfs)
         self.ui.splitButton.clicked.connect(partial(self.ui_action_handler, 'split'))
@@ -32,14 +36,13 @@ class MainWindow(QMainWindow):
         self.ui.rotateRightButton.clicked.connect(partial(self.ui_action_handler, 'rotate', 90))
         self.ui.cropButton.clicked.connect(self.open_checked_pdf_page_in_new_window)
         self.ui.trashButton.clicked.connect(partial(self.ui_action_handler, 'delete'))
-        self.ui.resetButton.clicked.connect(self.delete_push_button_from_grid)
+        self.ui.resetButton.clicked.connect(self.get_position_from_push_button_in_grid)
         self.ui.createPdfButton.clicked.connect(self.send_data_for_creating_pdf)
 
     def open_checked_pdf_page_in_new_window(self):
         self.is_push_button_checked()
         if len(self.checked_objects) is 1:
-            checked_object = self.checked_objects[0]
-            self.page_window = PdfPageWindow(checked_object, parent=self)
+            self.page_window = PdfPageWindow(self.checked_objects[0], parent=self)
             self.page_window.show()
 
     def dialog_to_select_pdfs(self):
@@ -74,15 +77,15 @@ class MainWindow(QMainWindow):
         self.is_push_button_checked()
         if action == 'change_position' and len(self.checked_objects) is 2:
             self.change_position_of_objects_ui()
-        for obj in self.checked_objects:
-            if action == 'delete':
-                self.page_objects.remove(obj)
-            elif action == 'rotate':
-                self.rotate_push_button_ui(obj, degree)
-            elif action == 'split':
-                self.split_push_button_ui(obj)
-        self.delete_push_button_from_grid()
-        self.position_push_button_in_grid()
+        else:
+            for obj in self.checked_objects:
+                if action == 'delete':
+                    self.page_objects.remove(obj)
+                elif action == 'rotate':
+                    self.rotate_push_button_ui(obj, degree)
+                elif action == 'split':
+                    self.split_push_button_ui(obj)
+        self.deleted_old_and_position_new_push_button_in_grid()
 
     def is_push_button_checked(self):
         self.checked_objects = []
@@ -97,28 +100,44 @@ class MainWindow(QMainWindow):
         obj.rotate_image_update_rotation_and_push_button(degree)
         obj.rotation += degree
 
-    def split_push_button_ui(self, obj):
-        second_obj = copy.copy(obj)
-        obj.split_left()
-        second_obj.split_right()
-        self.page_objects.insert(self.page_objects.index(obj)+1, second_obj)
+    def split_push_button_ui(self, object):
+        copy_of_object = copy.copy(object)
+        object.split_left()
+        copy_of_object.split_right()
+        self.page_objects.insert(self.page_objects.index(object) + 1, copy_of_object)
+
+    def deleted_old_and_position_new_push_button_in_grid(self):
+        self.get_position_from_push_button_in_grid()
+
+    def get_position_from_push_button_in_grid(self):
+        for push_button in reversed(range(self.ui.pushButtonGrid.count())):
+            self.button_to_remove = self.ui.pushButtonGrid.itemAt(push_button).widget()
+            self.delete_push_button_from_grid()
 
     def delete_push_button_from_grid(self):
-        for push_button in reversed(range(self.ui.pushButtonGrid.count())):
-            button_to_remove = self.ui.pushButtonGrid.itemAt(push_button).widget()
-            self.ui.pushButtonGrid.removeWidget(button_to_remove)
-            button_to_remove.setParent(None)
+        self.ui.pushButtonGrid.removeWidget(self.button_to_remove)
+        self.button_to_remove.setParent(None)
+        self.reset_grid_rows_and_columns()
+
+    def reset_grid_rows_and_columns(self):
+        self.row = 0
+        self.column = 0
+        self.position_push_button_in_grid()
 
     def position_push_button_in_grid(self):
-        row = 0
-        column = 0
         for page_object in self.page_objects:
-            self.ui.pushButtonGrid.addWidget(page_object.push_button, row, column)
-            column += 1
-            if int(len(self.page_objects) / 4) is column:
-                row += 1
-                column = 0
+            self.position_push_button_in_grid_row(page_object.push_button)
+            if int(len(self.page_objects) / 4) is self.column:
+                self.create_new_grid_row()
         self.ui.widgetLayout.setLayout(self.ui.pushButtonGrid)
+
+    def position_push_button_in_grid_row(self, push_button):
+        self.ui.pushButtonGrid.addWidget(push_button, self.row, self.column)
+        self.column += 1
+
+    def create_new_grid_row(self):
+        self.row += 1
+        self.column = 0
 
     def send_data_for_creating_pdf(self):
         self.logic.create_pdf_action_handler(self.page_objects)
