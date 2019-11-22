2a1b0311c0ecd7dd80e204a38645df7f169821f8
darjusch.schrand <Darjusch.schrand@bonial.com>
2019-11-22 14:40:41 +0100

    Cleaned and fixed bulk import for Pdf Folder

--- pdf_converter/main.py
+++ pdf_converter/main.py
@@ -1,4 +1,5 @@
 import copy
+import os
 import sys
 from functools import partial
 
@@ -28,6 +29,8 @@ class MainWindow(QMainWindow):
         self.button_to_remove = None
         self.row = 0
         self.column = 0
+        self.visible_from = 0
+        self.visible_until = 15
         self.ui.openFileButton.clicked.connect(self.dialog_to_select_pdfs)
         self.ui.openFolderButton.clicked.connect(self.dialog_to_select_folder_with_pdfs)
         self.ui.splitButton.clicked.connect(partial(self.ui_action_handler, 'split'))
@@ -36,8 +39,10 @@ class MainWindow(QMainWindow):
         self.ui.rotateRightButton.clicked.connect(partial(self.ui_action_handler, 'rotate', 90))
         self.ui.cropButton.clicked.connect(self.open_checked_pdf_page_in_new_window)
         self.ui.trashButton.clicked.connect(partial(self.ui_action_handler, 'delete'))
-        self.ui.resetButton.clicked.connect(partial(self.get_position_from_push_button_in_grid, self.page_objects.clear()))
+        self.ui.resetButton.clicked.connect(self.reset)
         self.ui.createPdfButton.clicked.connect(self.send_data_for_creating_pdf)
+        self.ui.rightButton.clicked.connect(partial(self.update_visible_button, "+"))
+        self.ui.leftButton.clicked.connect(partial(self.update_visible_button, "-"))
 
     def open_checked_pdf_page_in_new_window(self):
         self.is_push_button_checked()
@@ -48,15 +53,29 @@ class MainWindow(QMainWindow):
     def dialog_to_select_pdfs(self):
         pdf_dialog_obj = QFileDialog.getOpenFileNames(self, "Open Pdf", "/Downloads", "Pdf Files (*.pdf)",)
         self.pdf_path = pdf_dialog_obj[0][0]
-        self.setup()
+        self.open_and_read_pdf()
+        self.position_push_button_in_grid()
+
+    def update_visible_button(self, symbol):
+        if symbol is "+" and self.visible_until < len(self.page_objects):
+            self.visible_from += 15
+            self.visible_until += 15
+            self.deleted_old_and_position_new_push_button_in_grid()
+        elif symbol is "-" and self.visible_from is not 0:
+                self.visible_from -= 15
+                self.visible_until -= 15
+                self.deleted_old_and_position_new_push_button_in_grid()
+
+    def reset(self):
+        self.page_objects.clear()
+        self.get_position_from_push_button_in_grid()
 
     def dialog_to_select_folder_with_pdfs(self):
         path = QFileDialog.getExistingDirectory()
-        pass
-
-    def setup(self):
-        self.page_objects.clear()
-        self.open_and_read_pdf()
+        pdf_pathes = os.listdir(path)[1::]
+        for pdf in pdf_pathes:
+            self.pdf_path = f"{path}/{pdf}"
+            self.open_and_read_pdf()
         self.position_push_button_in_grid()
 
     def open_and_read_pdf(self):
@@ -114,11 +133,11 @@ class MainWindow(QMainWindow):
         for push_button in reversed(range(self.ui.pushButtonGrid.count())):
             self.button_to_remove = self.ui.pushButtonGrid.itemAt(push_button).widget()
             self.delete_push_button_from_grid()
+        self.reset_grid_rows_and_columns()
 
     def delete_push_button_from_grid(self):
         self.ui.pushButtonGrid.removeWidget(self.button_to_remove)
         self.button_to_remove.setParent(None)
-        self.reset_grid_rows_and_columns()
 
     def reset_grid_rows_and_columns(self):
         self.row = 0
@@ -126,9 +145,9 @@ class MainWindow(QMainWindow):
         self.position_push_button_in_grid()
 
     def position_push_button_in_grid(self):
-        for page_object in self.page_objects:
+        for page_object in self.page_objects[self.visible_from:self.visible_until]:
             self.position_push_button_in_grid_row_and_moves_to_next_column(page_object.push_button)
-            if int(len(self.page_objects) / 4) is self.column:
+            if 7 is self.column:
                 self.create_new_grid_row_and_sets_column_position_to_zero()
         self.ui.widgetLayout.setLayout(self.ui.pushButtonGrid)
 
